1b847f6012cdcb47c964ca21671c3a3cf7e4d8a3
Min0 <myyoon2484@gmail.com>
2025-09-20 14:32:21 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ MySQL/프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,1 +1,18 @@
 -- 코드를 작성해주세요
+-- 상관 서브쿼리
+# SELECT 
+#     p.ID,
+#     (SELECT COUNT(*) 
+#      FROM ECOLI_DATA c 
+#      WHERE c.PARENT_ID = p.ID) AS CHILD_COUNT
+# FROM ECOLI_DATA p
+# ORDER BY p.ID;
+
+-- 셀프 조인으로도 풀 수 있다고 함
+
+SELECT p.ID, COUNT(c.ID) AS CHILD_COUNT
+FROM ECOLI_DATA p
+LEFT JOIN ECOLI_DATA c ON p.ID = c.PARENT_ID
+-- self join시 하나의 부모 아이디에 여러개의 자식이 매치될 수 있기에 group by를 해준다.
+GROUP BY p.ID
+ORDER BY p.ID ASC;
